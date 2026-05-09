ca803184a0c6ab007069e49685cf54fc7de6e35a
aaazamatovich <jonibekuroqov777@gmail.com>
2026-05-09 13:33:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.BOT_TOKEN;
+const token = process.env.AAH6MVgTwqX8eu2MCdKFVhOAh2rGr-ZJGnA;
 
 const bot = new TelegramBot(token, { polling: true });
 
