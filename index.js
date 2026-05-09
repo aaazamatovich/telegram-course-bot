b101ec27970d1eada6d7ce3231c9d7203f7b9aea
aaazamatovich <jonibekuroqov777@gmail.com>
2026-05-09 13:27:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Bot ishladi");
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Salom Jonibek 😎 Bot ishladi!');
+});
+
+console.log("Bot ishga tushdi");
