8c1bd8d9304027190f82094a75e6d03e926f9a0c
aaazamatovich <jonibekuroqov777@gmail.com>
2026-05-09 15:25:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const TelegramBot = require('node-telegram-bot-api');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true
+  polling: true,
 });
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function runAI(text) {
+  const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash-latest',
+  });
+
+  const result = await model.generateContent(text);
+  const response = await result.response;
+  return response.text();
+}
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
   try {
-    const model = genAI.getGenerativeModel({
-      "model: "gemini-pro""
-    });
-
-    const result = await model.generateContent(text);
-
-    const response = result.response.text();
-
-    bot.sendMessage(chatId, response);
-
-  } catch (error) {
-    console.log(error);
-    bot.sendMessage(chatId, "Xatolik chiqdi 😢");
+    const answer = await runAI(text);
+    bot.sendMessage(chatId, answer);
+  } catch (err) {
+    console.log(err);
+    bot.sendMessage(chatId, 'AI ishlamayapti 😢');
   }
 });
